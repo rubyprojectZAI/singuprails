2650d9e8e023a71ed379c1582d340bb995c7edab
Jakub Krajewski <jakubjapuk@o2.pl>
2011-04-12 11:31:37 +0200

    log

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  	
+  	respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+    end
+  end
+  def index
+  end
+  def create
+  	
+  end
+
+end
