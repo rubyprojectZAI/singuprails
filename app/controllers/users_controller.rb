680d35b2ff0b53c6b2f0dd8913d83b71155d0481
Jakub Krajewski <jakubjapuk@o2.pl>
2011-04-12 17:59:15 +0200

    css+db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,28 @@ class UsersController < ApplicationController
     end
   end
   def index
+  	@users = User.all
+
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.xml  { render :xml => @users }
+    # end
   end
   def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		render "show"
+  	end
   	
+  	
+  end
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html  show.html.erb
+      format.xml  { render :xml => @user }
+    end
   end
 
 end
