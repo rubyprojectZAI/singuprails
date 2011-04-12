311cf202cc4493b76a2d95ac5a2123155efd387e
Jakub Krajewski <jakubjapuk@o2.pl>
2011-04-12 10:00:00 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signuprails::Application.config.secret_token = 'd7544847fdd8eb51e412200f8dc0932ed1d9704a4aea6630d97927957583e905914bffd30d15c386a7651a1ae85fa6759da4ef71cd74d78a88b524047dd200ab'
